b8b72a4a21baabe2c28ae778238a21a612eedf17
unknown <gustavocesar14798@gmail.com>
2020-03-23 19:58:57 -0300

    resolução dia 23-03

--- src/aula20200323/cofre/TestesComCofre.java
+++ src/aula20200323/cofre/TestesComCofre.java
@@ -7,8 +7,21 @@ import org.junit.jupiter.api.Test;
 class TestesComCofre {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void fecharAbrirCofre() {
+
+		Cofre cofreFechado = new Cofre();
+
+		assertEquals(false, cofreFechado.isAberto());
+
+		cofreFechado.abrir();
+		assertEquals(true, cofreFechado.isAberto());
+
+		cofreFechado.fechar();
+		assertEquals(false, cofreFechado.isAberto());
+
+		Cofre cofre = new Cofre();
+		cofre.abrir();
+		assertEquals(true, cofre.isAberto());
 	}
 
 }
